af04c3d6a93eb60d622967da1eeb280d0b35300b
anacirqueira <cirqueiraana6@gmail.com>
2022-03-20 21:29:13 -0300

    update APP

--- js/App.js
+++ js/App.js
@@ -10,5 +10,59 @@ export default class App {
     this._refreshNotes();
   }
 
-  
+   _refreshNotes() {
+     const notes = NotesApi.getAllNotes();
+
+     this.setNotes(notes);
+
+     if(notes.length > 0) {
+       this._setActiveNote(notes[0]);
+     }
+    
+    }
+
+    _setNotes(notes) {
+      this.notes = notes;
+      this.view.updateNoteList(notes);
+      this.view.updateNotePreviewVisibility(notes.length > 0);
+    }
+
+    _setActiveNote(note) {
+      this.activeNote = note;
+      this.view.updateActiveNote(note);
+    }
+
+    _handlers() {
+      return {
+        onNoteSelect: noteId => {
+          const selectedNote = this.notes.find(note => note.id == noteId);
+          this._setActiveNote(selectedNote);
+        },
+
+        onNoteAdd: () => {
+          const newNote = {
+            title: "New Note",
+            body: "Take note..."
+          };
+
+          NotesApi.saveNote(newNote);
+          this._refreshNotes();
+        },
+
+        onNoteEdit: (title, body) => {
+          NotesApi.saveNote({
+            id: this.activeNote.id,
+            title,
+            body
+          });
+
+          this._refreshNotes();
+        },
+
+        onNoteDelete: noteId => {
+          NotesApi.deleteNote(noteId);
+          this._refreshNotes();
+        },
+      };
+    }
 }
